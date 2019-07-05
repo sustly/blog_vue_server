07652ce32cee9c81b741fd8f27f6e6ce39750c84
sustly <279719732@qq.com>
2019-07-05 15:39:35 +0800

    loggerAspect

--- src/main/java/com/sustly/aspect/LoggerAspect.java
+++ src/main/java/com/sustly/aspect/LoggerAspect.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 /**
@@ -26,8 +25,8 @@ public class LoggerAspect {
 
     /**
      * 后置通知
-     * @param joinPoint
-     * @param retVal
+     * @param joinPoint joinPoint
+     * @param retVal retVal
      */
     @AfterReturning(pointcut = "pointCut()", argNames = "joinPoint, retVal", returning = "retVal")
     public void afterReturn(JoinPoint joinPoint, Object retVal) {
@@ -58,52 +57,6 @@ public class LoggerAspect {
 
     }
 
-
-    /**
-     * 使用Java反射来获取被拦截方法(insert、update)的参数值， 将参数值拼接为操作内容
-     *
-     * @param args
-     * @param mName
-     * @return
-     */
-    public String optionContent(Object[] args, String mName) {
-        if (args == null) {
-            return null;
-        }
-        StringBuilder rs = new StringBuilder();
-        rs.append(mName);
-        String className = null;
-        int index = 1;
-        // 遍历参数对象
-        for (Object info : args) {
-            // 获取对象类型
-            className = info.getClass().getName();
-            className = className.substring(className.lastIndexOf(".") + 1);
-            rs.append("[参数").append(index).append("，类型:").append(className).append("，值:");
-            // 获取对象的所有方法
-            Method[] methods = info.getClass().getDeclaredMethods();
-            // 遍历方法，判断get方法
-            for (Method method : methods) {
-                String methodName = method.getName();
-                // 判断是不是get方法
-                if (!methodName.contains("get")) {
-                    continue;
-                }
-                Object rsValue = null;
-                try {
-                    // 调用get方法，获取返回值
-                    rsValue = method.invoke(info);
-                } catch (Exception e) {
-                    continue;
-                }
-                // 将值加入内容中
-                rs.append("(").append(methodName).append(":").append(rsValue).append(")");
-            }
-            rs.append("]");
-            index++;
-        }
-        return rs.toString();
-    }
 
     /**
      * 判断操作的中文名
