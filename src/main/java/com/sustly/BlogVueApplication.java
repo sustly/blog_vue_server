17214374c8f8f9c50a6f18656676bcad6bc9b440
sustly <279719732@qq.com>
2019-07-11 10:27:39 +0800

    修改配置类以及pom文件可打成war包放入tomcat

--- src/main/java/com/sustly/BlogVueApplication.java
+++ src/main/java/com/sustly/BlogVueApplication.java
@@ -2,15 +2,21 @@ package com.sustly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author admin
  */
 @SpringBootApplication
-public class BlogVueApplication {
+public class BlogVueApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogVueApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogVueApplication.class);
+    }
 }
