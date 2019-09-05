8ff7d2f12f33dcb9f6410095c8c2ce6fafa55fe2
liyue <yue.li@ifchange.com>
2019-09-05 17:39:44 +0800

    上述问题解决，添加@EnableDiscoveryClient即可

--- blog_zuul/src/main/java/com/sustly/BlogZuul.java
+++ blog_zuul/src/main/java/com/sustly/BlogZuul.java
@@ -2,6 +2,7 @@ package com.sustly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class BlogZuul {
         public static void main(String[] args) {
