daa34d61719d54dd97ef8d511781b00394dad930
liyue <yue.li@ifchange.com>
2019-10-31 11:04:07 +0800

    中文乱码

--- src/main/java/com/sustly/config/DruidConfig.java
+++ src/main/java/com/sustly/config/DruidConfig.java
@@ -8,10 +8,16 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -64,6 +70,29 @@ public class DruidConfig {
         bean.setUrlPatterns(Collections.singletonList("/*"));
 
         return  bean;
+    }
+
+
+    /**
+     * 中文乱码解决
+     */
+    @Configuration
+    public class CharsetConfig extends WebMvcConfigurerAdapter {
+        @Bean
+        public HttpMessageConverter<String> responseBodyConverter() {
+            StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                    Charset.forName("UTF-8"));
+            return converter;
+        }
+        @Override
+        public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+            super.configureMessageConverters(converters);
+            converters.add(responseBodyConverter());
+        }
+        @Override
+        public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+            configurer.favorPathExtension(false);
+        }
     }
 
 }
