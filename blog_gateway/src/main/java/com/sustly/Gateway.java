08b49387f3b68ee68c6d2d060a87fd1d8468898f
liyue <yue.li@ifchange.com>
2020-03-17 13:40:51 +0800

    springcloud gateway

--- blog_gateway/src/main/java/com/sustly/Gateway.java
+++ blog_gateway/src/main/java/com/sustly/Gateway.java
@@ -3,18 +3,12 @@ package com.sustly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-/**
- * @Author: liyue
- * @Date: 19-9-5 上午9:28
- */
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
-public class BlogZuul{
+public class Gateway {
         public static void main(String[] args) {
-            SpringApplication.run(BlogZuul.class, args);
+            SpringApplication.run(Gateway.class, args);
         }
-
 }
+
